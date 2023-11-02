ec1d3c83fba22f84f76e8cffcbd4558d4ffa507f
Devsliger <114121700+Devsliger@users.noreply.github.com>
2023-11-02 13:40:40 +0100

    Update and rename slot_machine101 to generate_odds.py

---
+++ generate_odds.py
@@ -0,0 +1,40 @@
+
+import random
+
+ROWS = 3
+
+COLS = 3
+
+
+
+symbol_count = {
+                'A': 2,
+                'B': 4,
+                'C': 6,
+                'D': 8
+                }
+
+def get_slot_machine(cols, rows, symbols):
+    # Creates a list of symbols based on their counts
+    all_symbols = [symbol for symbol, count in symbols.items() for _ in range(count)]
+    # Arranges symbols in columns and rows randomly
+    columns = [[random.choice(all_symbols) for _ in range(rows)] for _ in range(cols)]
+    return columns
+
+
+def print_slot_machine(columns):
+    # Prints the slot machine grid
+    for row in range(len(columns[0])):
+        for column in columns:
+            print(column[row], end=" | ")
+        print()
+
+def main():
+    # Generates the slot machine
+    slots = get_slot_machine(ROWS, COLS, symbol_count)
+    # Prints the slot machine
+    print_slot_machine(slots)
+
+
+# Calls the main function to run the code
+main()
